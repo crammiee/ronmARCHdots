08fdd4a64d7f8bb41eba238ee4e805a3b509970d
crammiee <crammiee.business@gmail.com>
2026-06-26 11:02:03 +0800

    feat: added lazyvim options

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "FocusGained" }, {
+  command = "checktime",
+})
